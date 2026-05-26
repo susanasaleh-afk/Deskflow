0eadc9af62d1f582c193003e92568ad039be2c10
susanasaleh-afk <susana.saleh@gig.com>
2026-05-26 15:41:43 +0200

    Update recurring.js

--- api/recurring.js
+++ api/recurring.js
@@ -9,7 +9,7 @@ const supabase = createClient(
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://gig-deskflow.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-User-Id, X-User-Email, X-User-Name, X-Tenant-Id');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   let caller;
